3f4b65dac896a766dc7df95ebda1ff689dc981d3
Tjyy-66bd <1123351509@qq.com>
2022-07-05 21:06:22 +0800

    part3 code1

---
+++ GoBasic1/src/p3_GoContainer/code1_array.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func code1() {
+	var team [3]string = [3]string{"hammer", "solider", "mum"}
+
+	for k, v := range team {
+		fmt.Println(k, v)
+	}
+
+	for i := range team {
+		fmt.Println(i)
+	}
+}
+
+//func main() {
+//	code1()
+//}
